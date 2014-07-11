311ddb8a7b1cda8da22108263f5610e32934ccc8
Btibert3 <btibert3@gmail.com>
2014-07-11 19:22:22 -0400

    added Squared Euclid Distance to server

--- server.R
+++ server.R
@@ -1,9 +1,46 @@
 ## load the required packages for the app
 library(shiny)
-
+library(plyr)
 
 ## read in the database created with R/collect.R for the user's session
 rankings = readRDS(file="data/rankings-db.rds")
+
+## filter the data to what we will use in the app
+rankings = subset(rankings, select = c(INSTNM, 
+                                       ENRLT,
+                                       yield,
+                                       IGRNT_P,
+                                       NPGRN2,
+                                       RET_PCF,
+                                       STUFACR,
+                                       gradrate6))
+
+## Squared Euclidean distance for each school from the user's prefs
+## http://en.wikipedia.org/wiki/Euclidean_distance#Squared_Euclidean_distance
+calcUserPref = function(mydf, U_ENROLL = 975, 
+                              U_YIELD = .30, 
+                              U_IGRNT = 70, 
+                              U_NPG = 21000, 
+                              U_RET = 95, 
+                              U_SF = 12, 
+                              U_GR = 90) 
+{
+  ## calc the sum of squares 
+  sed =  (mydf$ENRLT - U_ENROLL)^2 + 
+        (mydf$yield - U_YIELD)^2 +
+        (mydf$IGRNT_P - U_IGRNT)^2 + 
+        (mydf$NPGRN2 - U_NPG)^2 + 
+        (mydf$RET_PCF - U_RET)^2 + 
+        (mydf$STUFACR - U_SF)^2 + 
+        (mydf$gradrate6 - U_GR)^2
+  
+  ## add the column to the dataframe
+  mydf$dist = sed
+  
+  ## create the rank, smaller values first
+  mydf = transform(mydf, rank = )
+  return(mydf)
+}
 
 
 # Define server logic required to draw a histogram
@@ -14,10 +51,11 @@ shinyServer(function(input, output, session) {
   
   ## keep the columns we want from the user rankings
   ## rep
-  user_df = subset(user_df, select = c(INSTNM, gradrate6, yield))
+
   
   
   ## apply distance function and return closest schools
+  final_df = 1
   
   
   ## the data for the table
